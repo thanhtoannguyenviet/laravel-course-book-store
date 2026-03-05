7cb5b04f28eb6abf5141fba555715b6d1e65c5bb
Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2026-03-05 21:26:12 +0700

    step3-fix-foreignkey

--- database/migrations/2026_03_03_172649_create_books_table.php
+++ database/migrations/2026_03_03_172649_create_books_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('books', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('title');
             $table->string('description')->nullable();
             $table->string('cover_url')->nullable();
